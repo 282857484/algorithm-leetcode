7f797907b3716b7488e0c04ec9e9a84dce1d562b
houbin <houbin@kingsoft.com>
2022-02-27 23:31:58 +0800

    。

---
+++ src/main/java/com/leecode/algorithm/easy/maxSubArray/Solution.java
@@ -0,0 +1,36 @@
+package com.leecode.algorithm.easy.maxSubArray;
+
+class Solution {
+    public static void main(String[] agrs) {
+        Solution s=new Solution();
+        int a = s.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        System.out.println(a);
+    }
+    /**
+     * 53. 最大子数组和
+     * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * 子数组 是数组中的一个连续部分。
+     *
+     * 示例 1：
+     * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出：6
+     * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        // 初始化动态规划数组
+        // dp[i]值的含义是以i为结尾的最大子数组和，可以理解为向下贡献值
+        int[] dp = new int[nums.length + 1];
+        dp[0] = - 999999;
+        for (int i = 0; i < nums.length; i++) {
+            // 一种情况是，前面的贡献值为无效值（负数）则取当前值为最大值
+            // 一种情况是，前面的贡献值为有效值则取当前值为最大值
+            dp[i+1] = Math.max(nums[i], nums[i] + dp[i]);
+            max = Math.max(max, dp[i+1]);
+        }
+        return max;
+    }
+}
--- src/main/java/com/leecode/algorithm/middle/maxProduct/Solution.java
+++ src/main/java/com/leecode/algorithm/middle/maxProduct/Solution.java
@@ -20,15 +20,17 @@ class Solution {
     public int maxProduct(int[] nums) {
 
         int max = Integer.MIN_VALUE;
-        // dp[i] 截止到i目前正数最大乘积
+        // dpmax[i]的值代表以i为结尾子序列最大乘积
         int[] dpmax = new int[nums.length];
-        // dp[i] 截止到i目前负数最小乘积
+        // dpmin[i]的值代表以i为结尾子序列最小乘积
         int[] dpmin = new int[nums.length];
         dpmax[0] = nums[0];
         dpmin[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
             // max与min函数可极大简化各种讨论情况，只需把可能的几种情况列出即可
             // 这里不需要讨论正负号，只需要考虑可能性
+            // 一种是最大值产生的贡献(最大值变大)，一种是最小值产生的贡献（负数*负数变大，反超最大值）
+            // 一种是之前以i为结尾子序列对现在没有贡献（比如i-1位置数值=0，i位置数值>0）
             dpmax[i] = Math.max(dpmax[i-1] * nums[i], Math.max(dpmin[i-1] * nums[i], nums[i]));
             dpmin[i] = Math.min(dpmax[i-1] * nums[i], Math.min(dpmin[i-1] * nums[i], nums[i]));
         }
