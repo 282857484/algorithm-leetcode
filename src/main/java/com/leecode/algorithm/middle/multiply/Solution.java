a6f56d0bb6607ec2cd93c6c975b8eaabf53a9462
BetLGD <1031248990@qq.com>
2022-03-10 22:36:18 +0000

    .

--- src/main/java/com/leecode/algorithm/middle/multiply/Solution.java
+++ src/main/java/com/leecode/algorithm/middle/multiply/Solution.java
@@ -1,28 +1,49 @@
 package com.leecode.algorithm.middle.multiply;
 
 public class Solution {
-    // TODO
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.multiply("1111","1234"));
+    }
+
+    // 计算类的进位不会弄就全扔外边
     public String multiply(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
-        // 数字从后往前放置，所以i从最后往前遍历
-        int[] res = new int[num1.length() + num2.length()];
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int n1 = num1.charAt(i) - '0';
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                int n2 = num2.charAt(j) - '0';
-                int sum = (res[i + j + 1] + n1 * n2);
-                res[i + j + 1] = sum % 10;
-                res[i + j] += sum / 10;
+        char[] n1 = num1.toCharArray();
+        char[] n2 = num2.toCharArray();
+        int a = num1.length() + num2.length();
+        int[] result = new int[num1.length() + num2.length() ];
+        for (int i = num1.length()-1; i >= 0; i--) {
+            int x = n1[i]-'0';
+            for (int j = num2.length()-1; j >= 0; j--) {
+                int y = n2[j]-'0';
+                int q = x*y % 10;
+                int p = x*y / 10;
+                // 低位
+                result[i+j+1] = result[i+j+1] + q;
+                // 高位
+                result[i+j] = result[i+j] + p;
             }
         }
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < res.length; i++) {
-            if (i == 0 && res[i] == 0) continue;
-            result.append(res[i]);
+
+        // 处理进位，不好处理进位就在外面进位！
+        int carry = 0;
+        for (int i = result.length-1; i >= 0; i--) {
+            int act = result[i]+carry;
+            result[i] = (act) % 10;
+            carry = act / 10;
         }
-        return result.toString();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < result.length; i++) {
+            if (i == 0 && result[i] == 0)
+                continue;
+            sb.append(result[i]);
+        }
+        return sb.toString();
+
     }
 
 
