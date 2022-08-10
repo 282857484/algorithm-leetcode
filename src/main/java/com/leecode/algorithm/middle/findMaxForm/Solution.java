d54c1baa7e540b8023b5ee810eeae6a564bcf4eb
houbin <houbin@kingsoft.com>
2022-08-10 19:15:51 +0800

    【01背包】474. 一和零

---
+++ src/main/java/com/leecode/algorithm/middle/findMaxForm/Solution.java
@@ -0,0 +1,74 @@
+package com.leecode.algorithm.middle.findMaxForm;
+
+/**
+ * 474. 一和零
+ * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+ * <p>
+ * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+ * <p>
+ * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+ * 输出：4
+ * 解释：最多有 5 个 0 和 3 个 1 的最大子集是 {"10","0001","1","0"} ，因此答案是 4 。
+ * 其他满足题意但较小的子集包括 {"0001","1"} 和 {"10","1","0"} 。{"111001"} 不满足题意，因为它含 4 个 1 ，大于 n 的值 3 。
+ * 示例 2：
+ * <p>
+ * 输入：strs = ["10", "0", "1"], m = 1, n = 1
+ * 输出：2
+ * 解释：最大的子集是 {"0", "1"} ，所以答案是 2 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= strs.length <= 600
+ * 1 <= strs[i].length <= 100
+ * strs[i] 仅由 '0' 和 '1' 组成
+ * 1 <= m, n <= 100
+ */
+public class Solution {
+    /**
+     * 01背包
+     * 将三维数组压缩为二维
+     * dp[i][j] = Math.max(dp[i - 1][j], dp[i - strms[i]][j - strns[i]] + 1);
+     * @param strs
+     * @param m
+     * @param n
+     * @return
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][0] = 0;
+        for (String s : strs) {
+            int zero = countZero(s);
+            int one = s.length() - zero;
+            for (int i = m; i >= zero; i--) {
+                for (int j = n; j >= one; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    private int countZero(String str) {
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '0') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        s.findMaxFormX(new String[]{"10","0001","111001","1","0"}, 5, 3);
+        s.findMaxForm(new String[]{"10","1","0"}, 1, 1);
+    }
+
+}
