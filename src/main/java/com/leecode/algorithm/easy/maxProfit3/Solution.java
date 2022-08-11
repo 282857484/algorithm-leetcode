0582244212ba674ffc1a4b0e486c3db92c6e7cd9
BetLGD <1031248990@qq.com>
2022-08-11 23:44:02 +0000

    ...

---
+++ src/main/java/com/leecode/algorithm/easy/maxProfit3/Solution.java
@@ -0,0 +1,79 @@
+package com.leecode.algorithm.easy.maxProfit3;
+
+/**
+ * 123. 买卖股票的最佳时机 III
+ * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+ *
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+ *
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入：prices = [3,3,5,0,0,3,1,4]
+ * 输出：6
+ * 解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+ *      随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+ * 示例 2：
+ *
+ * 输入：prices = [1,2,3,4,5]
+ * 输出：4
+ * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *      注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ *      因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * 示例 3：
+ *
+ * 输入：prices = [7,6,4,3,1]
+ * 输出：0
+ * 解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
+ * 示例 4：
+ *
+ * 输入：prices = [1]
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * 1 <= prices.length <= 105
+ * 0 <= prices[i] <= 105
+ */
+class Solution {
+    public static void main(String[] agrs) {
+        Solution s=new Solution();
+        int a = s.maxProfitIII(new int[]{3,3,5,0,0,3,1,4});
+        System.out.println(a);
+    }
+
+    /**
+     * 个人思路
+     * TODO
+     * 假设在i处第一次卖出，那么两次卖出就可以分为左边与右边
+     * dp[i]代表第一次交易从左到右的在i位置的最大利润
+     * dp[i] = dp[i-1] + max{0, dp[i-1] + prices[i]-prices[i-1]}
+     * pd[i] = pd[i-1] - min{0, pd[i-1] + prices[i]-prices[i-1]}
+     * 最大利润是 for i in [0:last] do  dp[i] - pd[i] 
+     * @param prices
+     * @return
+     */
+    public int maxProfit3(int[] prices) {
+        return 0;
+    }
+
+    public int maxProfitIII(int[] prices) {
+        // ????????????????傻逼玩意，不研究这个玩意了
+        int n = prices.length;
+        int buy1 = prices[0], sell1 = 0;
+        int buy2 = prices[0], sell2 = 0;
+        for (int i = 1; i < n; ++i) {
+            buy1 = Math.min(buy1, prices[i]);
+            sell1 = Math.max(sell1, prices[i] - buy1);
+            buy2 = Math.min(buy2, prices[i] - sell1);
+            sell2 = Math.max(sell2, prices[i] - buy2);
+        }
+        return sell2;
+    }
+
+
+}
