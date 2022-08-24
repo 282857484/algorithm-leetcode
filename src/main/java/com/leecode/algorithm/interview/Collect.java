352e9090b0a4b6dd2aef72ab0d867917f096cb3f
houbin <houbin@kingsoft.com>
2022-08-24 11:30:20 +0800

    。

--- src/main/java/com/leecode/algorithm/interview/Collect.java
+++ src/main/java/com/leecode/algorithm/interview/Collect.java
@@ -1,15 +1,22 @@
 package com.leecode.algorithm.interview;
 
-// 排序
+// 各种排序
 import com.leecode.algorithm.middle.sortArray.*;
+// 删除倒数第n个
 import com.leecode.algorithm.middle.removeNthFromEnd.*;
+// 环的第一个节点
 import com.leecode.algorithm.middle.detectCycle.*;
+// 检测环
 import com.leecode.algorithm.easy.hasCycle.*;
+// 前 K 个高频元素
 import com.leecode.algorithm.middle.topKFrequent.*;
+// 最小的k个数 （堆）
 import com.leecode.algorithm.easy.getLeastNumbers.*;
 import com.leecode.algorithm.node.TreeNode;
+// 二叉树的层序遍历
 import com.leecode.algorithm.middle.levelOrder.*;
 import com.leecode.algorithm.easy.maxSubArray.*;
+// 还原ip
 import com.leecode.algorithm.middle.restoreIpAddresses.*;
 // 模式匹配
 import com.leecode.algorithm.middle.patternMatching.*;
@@ -19,7 +26,7 @@ import com.leecode.algorithm.easy.convertToBase7.*;
 import com.leecode.algorithm.hard.isMatch.*;
 // 马拉车
 import com.leecode.algorithm.middle.longestPalindrome.Solution6;
-
+// 缓冲
 import com.leecode.algorithm.node.LRUCache;
 import com.leecode.algorithm.node.MyHashMap;
 // 链表
@@ -32,6 +39,13 @@ import com.leecode.algorithm.node.Heap;
 // https://blog.csdn.net/Y_yibao/article/details/103822000
 // 重排链表
 import com.leecode.algorithm.middle.reorderList.*;
+
+/**
+ * 开关灯泡
+ * 题目：初始时有 n 个灯泡关闭。 第 1 轮，你打开所有的灯泡。 第 2 轮，每两个灯泡你关闭一次。
+ * 第 3 轮，每三个灯泡切换一次开关（如果关闭则开启，如果开启则关闭）。
+ * 第 i 轮，每 i 个灯泡切换一次开关。 对于第 n 轮，你只切换最后一个灯泡的开关。 找出 n 轮后有多少个亮着的灯泡。
+ */
 
 public class Collect {
 
